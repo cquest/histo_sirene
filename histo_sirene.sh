1b1546c2050791374c6ef8a07f26d22ad319efab
cquest <cquest@openstreetmap.fr>
2018-03-02 12:35:00 +0100

    SIRETPS (old) before SIRET_NEW

--- histo_sirene.sh
+++ histo_sirene.sh
@@ -13,5 +13,5 @@
 unzip -p $1 | iconv -f cp1252 -t utf8 |
 csvcut -c SIREN,DATEMAJ,NIC,SIRETPS,NICSIEGE,VMAJ,EVE -d ';' -v| \
 egrep '(^SIREN|,(CTE|CTS|MTDE|MTAE|MTDS|MTAS|STE|STS|SU)$)' | \
-csvsql --query "select o.siren||substr('0000' || o.nic, -5, 5) as SIRET_OLD, n.siren||substr('0000' || n.nic, -5, 5) as SIRET_NEW, o.datemaj from stdin o join stdin n on (o.siren=n.siren and o.datemaj=n.datemaj and o.nic<n.nic) union select siren||substr('0000' || nic, -5, 5) as SIRET_NEW, siretps, datemaj from stdin where siretps <> '' and cast(siren as text) <> substr(siretps, 0, 10);" > ${1/.zip}-histo.csv
+csvsql --query "select o.siren||substr('0000' || o.nic, -5, 5) as SIRET_OLD, n.siren||substr('0000' || n.nic, -5, 5) as SIRET_NEW, o.datemaj from stdin o join stdin n on (o.siren=n.siren and o.datemaj=n.datemaj and o.nic<n.nic) union select siretps as SIRET_OLD, siren||substr('0000' || nic, -5, 5) as SIRET_NEW, datemaj from stdin where siretps <> '' and cast(siren as text) <> substr(siretps, 0, 10);" > ${1/.zip}-histo.csv
 
