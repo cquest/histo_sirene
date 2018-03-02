f7d06bad427b3f5dab6ff15bafbd47ee0079278f
cquest <cquest@openstreetmap.fr>
2018-03-02 12:30:49 +0100

    silent cron + keychain for rsync

--- cron.sh
+++ cron.sh
@@ -1,5 +1,5 @@
 # récupération des nouveaux fichiers
-for f in $(curl 'http://files.data.gouv.fr/sirene/' -s | grep 'sirene_[0-9]*_E_Q.zip' -o); do wget http://files.data.gouv.fr/sirene/$f -nc; done
+for f in $(curl 'http://files.data.gouv.fr/sirene/' -s | grep 'sirene_[0-9]*_E_Q.zip' -o); do wget http://files.data.gouv.fr/sirene/$f -nc -q ; done
 
 # extraction des changements de SIRET
 for f in *.zip
@@ -11,5 +11,9 @@ do
   fi
 done
 
+# fichier cumulé sur l'année
+csvstack sirene_2018*.csv > histo_sirene_2018.csv
+
 # publication...
+. ~/.keychain/cquest-Precision-WorkStation-T7500-sh
 rsync *.csv root@sc1.cquest.org:/var/www/html/histo_sirene -av
